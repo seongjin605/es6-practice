5be34f48828e1c7e0c7f75bd6de04a07f9d31cf2
preciouStar <seongjin605@users.noreply.github.com>
2020-03-19 00:32:59 +0900

    See MSDN link for destructuring

--- destructuring/index.js
+++ destructuring/index.js
@@ -5,14 +5,14 @@
 
 // 서버에서 만약 response에서 notification가 없다는 최악의 상황을 가정해보자.
 const response = {
-  // notifications: {
-  //     follow: true,
-  //     alerts: true,
-  //     unfollow: false
-  // },
-  user: {
-    lastName: "park"
-  }
+    // notifications: {
+    //     follow: true,
+    //     alerts: true,
+    //     unfollow: false
+    // },
+    user: {
+        lastName: 'park'
+    }
 };
 
 // 팔로우가 true 일 때, 이메일을 보낸다고 할 때,
@@ -27,29 +27,32 @@ const response = {
 const { notifications: { follow = false } = {} } = response;
 
 if (follow) {
-  console.log("sending email...");
+    console.log('sending email...');
 } else {
-  console.log("not follow or server problem");
+    console.log('not follow or server problem');
 }
 // 위 방식을 개선한 problem 작성하기.
 // Destructuring 방식으로 notifications나 내부에 있는 follow가 없는걸 대처.
 
+/**
+ *  @see 선언없는할당: https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+ */
 let followOfNotifications;
 let userOfNotifications;
 
 ({
-  notifications: { follow: followOfNotifications = false } = {},
-  user: { follow: userOfNotifications = false } = {}
+    notifications: { follow: followOfNotifications = false } = {},
+    user: { follow: userOfNotifications = false } = {}
 } = response);
 
 if (followOfNotifications) {
-  console.log(`notifications의 follow 상태는: ${followOfNotifications}`);
+    console.log(`notifications의 follow 상태는: ${followOfNotifications}`);
 } else {
-  console.log(`notifications의 follow 상태는: ${followOfNotifications}`);
+    console.log(`notifications의 follow 상태는: ${followOfNotifications}`);
 }
 
 if (userOfNotifications) {
-  console.log(`user의 follow 상태는: ${userOfNotifications}`);
+    console.log(`user의 follow 상태는: ${userOfNotifications}`);
 } else {
-  console.log(`user의 follow 상태는: ${userOfNotifications}`);
+    console.log(`user의 follow 상태는: ${userOfNotifications}`);
 }
