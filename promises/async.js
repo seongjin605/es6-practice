0e2577a5e25409518aaf64d39c61979158cc651c
jin.park <jin.park@asurion.com>
2023-02-07 09:37:13 +0900

    update

--- promises/async.js
+++ promises/async.js
@@ -1,33 +1,15 @@
-// const sleep = t => {
-//     return new Promise(resolve => setTimeout(resolve, t));
-// };
-
-// const start = async () => {
-//     const startDateTime = new Date().getTime();
-//     for (let i = 1; i <= 5; i++) {
-//         await sleep(1000);
-//         console.log(i);
-//     }
-//     const endDateTime = new Date().getTime();
-//     console.log((endDateTime - startDateTime) / 1000 + '초');
-// };
-
-// start();
-
 const sleep = t => {
-    return setTimeout(() => t);
-    // return new Promise(resolve => setTimeout(resolve, t));
+  return new Promise(resolve => setTimeout(resolve, t));
 };
 
-function start() {
-    const startDateTime = new Date().getTime();
-
-    for (let i = 0; i < 1000000; i++) {
-        // sleep(1000);
-        console.log(i);
-    }
-    const endDateTime = new Date().getTime();
-    console.log((endDateTime - startDateTime) / 1000 + '초');
-}
+const start = async () => {
+  const startDateTime = new Date().getTime();
+  for (let i = 1; i <= 5; i++) {
+    await sleep(1000);
+    console.log(i);
+  }
+  const endDateTime = new Date().getTime();
+  console.log((endDateTime - startDateTime) / 1000 + '초');
+};
 
 start();
